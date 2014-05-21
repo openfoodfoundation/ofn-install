5ff4b8a5939e01bb3f46655cabd4723ec64009a6
Rafael Schouten <rafaelschouten@gmail.com>
2014-05-21 20:03:58 +0100

    update var names

--- roles/webserver/templates/unicorn.rb
+++ roles/webserver/templates/unicorn.rb
@@ -1,8 +1,8 @@
-working_directory "{{ app_path }}"
+working_directory "{{ current_path }}"
 pid "{{ unicorn_pid }}"
 stderr_path "{{ unicorn_log }}"
 stdout_path "{{ unicorn_log }}"
 
-listen "/tmp/unicorn.{{ application }}.sock"
+listen "/tmp/unicorn.{{ app }}.sock"
 worker_processes {{ unicorn_workers }}
 timeout {{ unicorn_timeout }}
